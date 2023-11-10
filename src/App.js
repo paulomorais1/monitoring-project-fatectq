6e0cf61de4bcabb68d59bb18eaba43d7c53dad87
Paulo César de Sousa Morais <paulumora@hotmail.com>
2023-11-09 21:35:28 -0300

    feat create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header';
-import Footer from './components/footer';
-import HomeContainer from './containers/HomeContainer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/shared/header";
+import Footer from "./components/shared/footer";
+import HomeContainer from "./containers/HomeContainer";
 // import MonitorDetailsContainer from './containers/MonitorDetailsContainer';
-import './App.css'; 
+import "./App.css";
 
 const App = () => {
   return (
